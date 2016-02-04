8ffd1499edb406a3e82723ff86d49cb9d0fe5377
Chris Eady <chris.eady1@gmail.com>
2016-02-04 16:59:48 -0600

    Players pick names, symbols, and make one move.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -33,16 +33,53 @@ class Board
     end
   end
 
+  def update_board(location)
+    board[location[1]][location[0]].piece = location[2]
+    display_board
+  end
 end
 
 class Player
-  attr_reader :name
-  def initialize(name)
-    @name = name
+  attr_reader :name, :symbol
+  def initialize
+    @name = get_player_name
+    @symbol = choose_symbol
   end
 
-  
+  private
+
+  def get_player_name
+    gets.chomp
+  end
+
+  def choose_symbol
+    puts "What symbol would you like to use?"
+    gets.chomp
+  end
+
+  public
+
+  def take_turn
+    print "#{name}, please enter your next move >> "
+    location = gets.chomp
+    column = location[0].upcase.ord - 65
+    row = (location[1].to_i - 3).abs # TODO: Change hardcoded calculation to reference row count.
+    [column, row, self.symbol]
+  end
 
 end
 
 my_board = Board.new(3,3)
+
+puts "What is Player 1's name?"
+player1 = Player.new()
+puts "Hi, #{player1.name}! You've chosen to use #{player1.symbol}."
+
+puts "What is Player 2's name?"
+player2 = Player.new()
+puts "Hi, #{player2.name}! You've chosen to use #{player2.symbol}."
+
+loop do
+  my_board.update_board(player1.take_turn)
+  my_board.update_board(player2.take_turn)
+end
