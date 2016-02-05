241a9ac53c124ac0a94a4eccf7685afdcdc3b998
Chris Eady <chris.eady1@gmail.com>
2016-02-05 12:20:37 -0600

    corrected win? check errors and edited whitespace in user prompts throughout application

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,14 +1,16 @@
 class Board
-  attr_reader :board
+  attr_reader :board, :blank_square_string
   def initialize(number_of_rows, number_of_columns)
     @number_of_rows = number_of_rows
     @number_of_columns = number_of_columns
+    @blank_square_string = '-'
 
     @board = create_board
     display_board
   end
 
   Square = Struct.new(:piece)
+
 
   private
 
@@ -20,40 +22,51 @@ class Board
 
   def create_row_of_squares
     squares = []
-    @number_of_columns.times { squares << Square.new('--') }
+    @number_of_columns.times { squares << Square.new(blank_square_string) }
     squares
   end
 
   public
 
   def win?
-    p 'win? called'
     game_over = false
-    board.each do |row|
-      p row
-      if row.uniq.size == 1 then return true end
-    end
-    board.transpose.each do |column|
-      p column
-      if column.uniq.size == 1 then return true end
+    board.each do |row| # Check rows for win.
+      if row.uniq.size == 1 && row.uniq[0].piece != blank_square_string
+        game_over = true
+      end
     end
 
-    if board.flatten.values_at(0,3,6).uniq.size == 1 then game_over = true end
-    if board.flatten.values_at(2,5,8).uniq.size == 1 then game_over = true end
+    board.transpose.each do |column| # Check columns for win.
+      if column.uniq.size == 1 && column.uniq[0].piece != blank_square_string
+        game_over = true
+        p game_over
+      end
+    end
+
+    # Check diagonals for win.
+    if board.flatten.values_at(0,4,8).uniq.size == 1 && board.flatten.values_at(0,4,8).uniq[0].piece != blank_square_string
+      game_over = true
+    end
+    if board.flatten.values_at(2,4,6).uniq.size == 1 && board.flatten.values_at(2,4,6).uniq[0] != blank_square_string
+      game_over = true
+    end
 
     game_over
   end
 
   def display_board
-    board.each do |row|
-      row.each { |square| print "|#{square.piece}|" }
+    print "\n"
+    board.each_with_index do |row, index|
+      print "#{index + 1} "
+      row.each { |square| print "#{square.piece} " }
       print "\n"
     end
+    print "  A B C"
+    print "\n"
   end
 
   def update_board(move)
     board[move[1]][move[0]].piece = move[2]
-    display_board
   end
 
 end
@@ -72,14 +85,14 @@ class Player
   end
 
   def choose_symbol
-    puts "What symbol would you like to use?"
+    print "\nHi, #{name}! Please enter the symbol would you like to use. >> "
     gets.chomp
   end
 
   public
 
   def take_turn
-    print "#{name}, please enter your next move >> "
+    print "\n#{name}, please enter your next move. >> "
     location = gets.chomp
     column = location[0].upcase.ord - 65
     row = (location[1].to_i - 3).abs # TODO: Change hardcoded calculation to reference row count.
@@ -90,20 +103,29 @@ end
 
 my_board = Board.new(3,3)
 
-puts "What is Player 1's name?"
+print "\nPlayer 1, please type your name. >> "
 player1 = Player.new()
-puts "Hi, #{player1.name}! You've chosen to use #{player1.symbol}."
+puts "\n#{player1.name}, you've chosen to use #{player1.symbol}."
 
-puts "What is Player 2's name?"
+print "\nPlayer 2, please type your name. >> "
 player2 = Player.new()
-puts "Hi, #{player2.name}! You've chosen to use #{player2.symbol}."
+puts "\n#{player2.name}, you've chosen to use #{player2.symbol}."
+
+current_player ||= player1
 
 loop do
-  move = player1.take_turn
+  my_board.display_board
+  move = current_player.take_turn
   my_board.update_board(move)
-  move = player2.take_turn
-  my_board.update_board(move)
+
   break if my_board.win? == true
+
+  if current_player == player1
+    current_player = player2
+  else
+    current_player = player1
+  end
 end
 
-puts "The game is over. Somebody won!"
+my_board.display_board
+puts "\nThe game is over. #{current_player.name} won!\n"
